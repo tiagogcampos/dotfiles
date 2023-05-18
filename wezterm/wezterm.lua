8a0b1068ed7d1376ef59ee507f4faf932d0d8d7b
Tiago Campos <13822700+tiagogcampos@users.noreply.github.com>
2023-05-18 21:26:05 +0100

    commit config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require("wezterm")
+
+local config = {}
+
+config.color_scheme = "Catppuccin Mocha"
+
+config.font = wezterm.font 'SF Mono Nerd Font'
+config.font_size = 16.0
+
+config.enable_tab_bar = false
+
+return config
