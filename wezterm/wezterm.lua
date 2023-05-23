1c8e4dd24f5e9f3735688d30a5f9083c229fddac
Tiago Campos <13822700+tiagogcampos@users.noreply.github.com>
2023-05-23 23:34:08 +0100

    update config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,9 +2,9 @@ local wezterm = require("wezterm")
 
 local config = {}
 
-config.color_scheme = "Catppuccin Mocha"
-
-config.font = wezterm.font 'SF Mono Nerd Font'
+-- config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "tokyonight_moon"
+config.font = wezterm.font "SF Mono Nerd Font"
 config.font_size = 16.0
 
 config.enable_tab_bar = false
